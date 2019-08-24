911842ea3c6099b321c2123cdf4f0f9e5b631ebf
Marty McFly <mirko.maya@live.com>
2019-08-24 11:08:13 -0400

    Added click event to app.js.

--- app.js
+++ app.js
@@ -1,58 +1,37 @@
 $(document).ready(function () {
 
-
-    $("#submit").on("click", function() {
-
-        var newRow = $("<div class='row'>");
-
-        
-
-
-
-        
-
-
-
-
-
-
-
-    })
-
-    function createRow() {
-
-
-
-
-    }
-
-
-    function grabInfo() {
+    $("#submit-input").on("click", function() {
 
         var employee = $("#employee-input").val().trim();
 
         var role = $("#role-input").val().trim();
 
-        var startDate = $("#start-input")
+        var start = $("#start-input").val().trim();
 
+        var months = $("#months-input").val().trim();
 
+        var rate = $("#rate-input").val().trim();
 
+        var bill = $("#bill-input").val().trim();
 
+        var tRow = $("<tr>");
 
-    }
+        var employeeTd = $("<td>").text(employee);
 
+        var roleTd = $("<td>").text(role);
 
+        var startTd = $("<td>").text(start);
 
+        var monthsTd = $("<td>").text(months);
 
+        var rateTd = $("<td>").text(rate);
 
+        var billTd = $("<td>").text(bill);
 
+        tRow.append(employeeTd, roleTd, startTd, monthsTd, rateTd, billTd);
 
+        $("tbody").append(tRow);
 
-
-
-
-
-
-
+    })
 
 })
