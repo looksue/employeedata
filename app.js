73099cb8e0d0a56433389f1dc82860a57041be72
Marty McFly <mirko.maya@live.com>
2019-08-24 10:51:45 -0400

    Added app.js file.

---
+++ app.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+
+    $("#submit").on("click", function() {
+
+        var newRow = $("<div class='row'>");
+
+        
+
+
+
+        
+
+
+
+
+
+
+
+    })
+
+    function createRow() {
+
+
+
+
+    }
+
+
+    function grabInfo() {
+
+        var employee = $("#employee-input").val().trim();
+
+        var role = $("#role-input").val().trim();
+
+        var startDate = $("#start-input")
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
