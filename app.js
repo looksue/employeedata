f782261bcf666187084f36f8403e3b355f71743e
Marty McFly <mirko.maya@live.com>
2019-08-24 12:04:08 -0400

    Added firebase.

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
 $(document).ready(function () {
 
-    $("#submit-input").on("click", function() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyCHW95eLcuj-Iq4Q3T1w2lu_Km3nOm4vO4",
+        authDomain: "empdata-bac20.firebaseapp.com",
+        databaseURL: "https://empdata-bac20.firebaseio.com",
+        projectId: "empdata-bac20",
+        storageBucket: "",
+        messagingSenderId: "313452622833",
+        appId: "1:313452622833:web:0a10bb18c5aed997"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
+    var database = firebase.database();
+
+    var employeeName = "";
+    var email = "";
+    var age = 0;
+    var comment = "";
+
+    $("#submit-input").on("click", function () {
+
+        event.preventDefault();
+
+        // Grabbed Values
         var employee = $("#employee-input").val().trim();
-
         var role = $("#role-input").val().trim();
-
         var start = $("#start-input").val().trim();
-
         var months = $("#months-input").val().trim();
-
         var rate = $("#rate-input").val().trim();
-
         var bill = $("#bill-input").val().trim();
 
-        var tRow = $("<tr>");
+        database.ref().set({
+            name: employee,
+            role: role,
+            start: start,
+            months: months,
+            rate: rate,
+            bill: bill,
+        });
 
-        var employeeTd = $("<td>").text(employee);
+        // var tRow = $("<tr>");
 
-        var roleTd = $("<td>").text(role);
+        // var employeeTd = $("<td>").text(employee);
 
-        var startTd = $("<td>").text(start);
+        // var roleTd = $("<td>").text(role);
 
-        var monthsTd = $("<td>").text(months);
+        // var startTd = $("<td>").text(start);
 
-        var rateTd = $("<td>").text(rate);
+        // var monthsTd = $("<td>").text(months);
 
-        var billTd = $("<td>").text(bill);
+        // var rateTd = $("<td>").text(rate);
 
-        tRow.append(employeeTd, roleTd, startTd, monthsTd, rateTd, billTd);
+        // var billTd = $("<td>").text(bill);
 
-        $("tbody").append(tRow);
+        // tRow.append(employeeTd, roleTd, startTd, monthsTd, rateTd, billTd);
+
+        // $("tbody").append(tRow);
 
     })
 
