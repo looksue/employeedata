fb3b36bdc835cac9006ddc12374a6128247f15c3
Marty McFly <mirko.maya@live.com>
2019-08-24 12:55:25 -0400

    Added .push and child -added.

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ $(document).ready(function () {
 
     var database = firebase.database();
 
-    var employeeName = "";
-    var email = "";
-    var age = 0;
-    var comment = "";
-
     $("#submit-input").on("click", function () {
 
         event.preventDefault();
@@ -31,14 +26,50 @@ $(document).ready(function () {
         var rate = $("#rate-input").val().trim();
         var bill = $("#bill-input").val().trim();
 
-        database.ref().set({
+        // database.ref().set({
+        //     name: employee,
+        //     role: role,
+        //     start: start,
+        //     months: months,
+        //     rate: rate,
+        //     bill: bill,
+        // });
+
+        database.ref().push({
             name: employee,
             role: role,
             start: start,
             months: months,
             rate: rate,
             bill: bill,
+            dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
+
+        // Firebase watcher .on("child_added"
+        database.ref().on("child_added", function (snapshot) {
+            // storing the snapshot.val() in a variable for convenience
+            var sv = snapshot.val();
+
+            // Console.loging the last user's data
+            console.log(sv.name);
+            console.log(sv.role);
+            console.log(sv.start);
+            console.log(sv.months);
+            console.log(sv.rate);
+            console.log(sv.bill);
+            console.log(sv.dateAdded);
+
+            // Change the HTML to reflect
+            // $("#name-display").text(sv.name);
+            // $("#email-display").text(sv.email);
+            // $("#age-display").text(sv.age);
+            // $("#comment-display").text(sv.comment);
+
+            // Handle the errors
+        }, function (errorObject) {
+            console.log("Errors handled: " + errorObject.code);
+        });
+
 
         // var tRow = $("<tr>");
 
@@ -59,5 +90,17 @@ $(document).ready(function () {
         // $("tbody").append(tRow);
 
     })
+
+    // const monthDifference = moment(new Date(endDate)).diff(new Date(startDate), 'months', true);
+    // function getAbsoulteMonths(momentDate) {
+    //     var months = Number(momentDate.format("MM"));
+    //     var years = Number(momentDate.format("YYYY"));
+    //     return months + (years * 12);
+    // }
+
+    // var startMonths = getAbsoulteMonths(start);
+    // var endMonths = getAbsoulteMonths(end);
+
+    // var monthDifference = endMonths - startMonths;
 
 })
